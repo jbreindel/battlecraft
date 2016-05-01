69452460c4700d86528dd7ee6853a8c9ecb14949
jake <j.breindel2@outlook.com>
2016-05-01 12:19:40 -0400

    Added some more code for game server.

--- apps/bc_game/src/bc_game_serv.erl
+++ apps/bc_game/src/bc_game_serv.erl
@@ -4,14 +4,21 @@
 
 %% api functions
 -export([start_link/0,
-		 create_game/0,
-		 join_game/1
+		 create_game/1,
+		 join_game/1,
+		 get_game/1
 		]).
 
 %% gen_server callbacks
 -export(init/1,
 		handle_call/3,
 		handle_cast/2).
+
+%% state rec
+-record(state, {
+				sup,
+				active_games
+				}).
 
 %%====================================================================
 %% API functions
@@ -23,4 +30,9 @@ start_link() ->
 create_game(Privacy) ->
 	gen_server:call(bc_game_serv, {create_game, Privacy}).
 
+%%====================================================================
+%% Gen_server callbacks
+%%====================================================================
 
+%% handle_call({create_game, Privacy}, _From, S = #state{sup = Sup, active_games = Games}) ->
+%% 	{reply, {ok, }}
