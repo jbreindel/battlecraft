79c1f8b0c1c81bc44ddbca65019bb034c77b5475
jake <j.breindel2@outlook.com>
2016-06-08 00:00:12 -0400

    Added a bc_collision module for bc collisions via an ets table.

--- apps/bc_map/src/bc_collision.erl
+++ apps/bc_map/src/bc_collision.erl
@@ -1,6 +1,32 @@
 
 -module(bc_collision).
 
-create(GameId) ->
-	mnesia:create_table(GameId, [{ram_copies, [node()], 
-		{storage_properties, [{ets, {write_concurrency, true}}]}]).
+%% ets table options
+-define(ETS_OPTIONS(BcGameSup), [set, 
+								 public, 
+								 {heir, BcGameSup, []}, 
+								 {write_concurrency, true}]).
+
+-spec create(BcGameSup :: pid(), 
+			 GameId :: integer()) -> ets:tid().
+create(BcGameSup) ->
+	ets:new(collision, ?ETS_OPTIONS(BcGameSup)).
+
+-spec insert(Tab :: ets:tid(),
+			 CollisionMap :: bc_collision_serv:collision()) -> true.
+insert(Tab, CollisionMap) ->
+	Rows = vertex_rows(CollisionMap),
+	ets:insert(Tab, Rows).
+
+-spec insert_new(Tab :: ets:tid(),
+				 CollisionMap :: bc_collision_serv:collision()) -> boolean().
+insert_new(Tab, CollisionMap) ->
+	Rows = vertex_rows(CollisionMap),
+	ets:insert_new(Tab, Rows).
+
+%% update(Tab, OriginalCollisionMap, UpdatedCollisionMap) ->
+%% 	
+	
+vertex_rows(#{id := Id, vertices := Vertices} = CollisionMap) ->
+	lists:map(fun(Vertex) -> {Vertex, Id} end, Vertices).													  
+	
