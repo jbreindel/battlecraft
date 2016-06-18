fa986b2b072b27a6d4a07115823553b120c43de1
jake <j.breindel2@outlook.com>
2016-06-18 14:39:47 -0400

    Added function specs.

--- apps/bc_web/src/bc_web_files.erl
+++ apps/bc_web/src/bc_web_files.erl
@@ -7,17 +7,22 @@
 		 static_dir/0, 
 		 assets_dir/0]).
 
+-spec routes_file() -> string().
 routes_file() ->
 	filename:join([code:priv_dir(bc_web), "bc_web.routes"]).
 
+-spec views_dir() -> string().
 views_dir() ->
 	filename:join([code:priv_dir(bc_web), "views"]).
 
+-spec view_file(ViewFile :: string()) -> string().
 view_file(ViewFile) ->
 	filename:join([views_dir(), ViewFile]).
 
+-spec static_dir() -> string().
 static_dir() ->
 	filename:join([code:priv_dir(bc_web), "static"]).
-	
+
+-spec assets_dir() -> string().
 assets_dir() ->
 	filename:join([static_dir(), "assets"]).
