7d3e5fdbb8b9d89a11154a7acb5622020530d9ef
jake <j.breindel2@outlook.com>
2016-06-18 13:34:59 -0400

    Renamed landing.html to index.html to be more standard.

--- apps/bc_web/src/bc_web_landing.erl
+++ apps/bc_web/src/bc_web_landing.erl
@@ -4,7 +4,7 @@
 -export([init/3, handle/2, terminate/3]).
 
 init(_Type, Req, _Opts) ->
-	ViewFile = bc_web_files:view_file("landing.html"),
+	ViewFile = bc_web_files:view_file("index.html"),
 	erlydtl:compile(ViewFile, landing_view, 
 					[{vars, [{title, "Battlecraft Online"}, 
 							 {description, "An online real time strategy simulation game."}]}]),
