099a8f043cd7995aab6401300b79456c8090b0c9
jake <j.breindel2@outlook.com>
2016-06-26 14:00:53 -0400

    Added more code to the join module.

--- apps/bc_web/priv/static/elm/Join.elm
+++ apps/bc_web/priv/static/elm/Join.elm
@@ -1,9 +1,26 @@
+port module Join exposing (..)
+
 import Html exposing (..)
 import Html.App as App
 import Html.Attributes exposing (..)
+import WebSocket
 
+-- Model
+
+type alias Model = {
+	address : String
+	playerId : Int
+}
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+	WebSocket.listen model.address NewMessage
+
+main : Program(Maybe Model)
 main =
-	App.programWithFlags
-	{
-
+	App.programWithFlags {
+		init = init,
+		view = view,
+		update = update,
+		subscriptions = subscriptions
 	}
