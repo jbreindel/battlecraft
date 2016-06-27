3704e96856ae354ef9eb3c9971c43dbe8a43a9c8
jake <j.breindel2@outlook.com>
2016-06-27 18:03:11 -0400

    Added more code to the Join program.

--- apps/bc_web/priv/static/elm/Join.elm
+++ apps/bc_web/priv/static/elm/Join.elm
@@ -3,6 +3,8 @@ port module Join exposing (..)
 import Html exposing (..)
 import Html.App as App
 import Html.Attributes exposing (..)
+import Json.Decode exposing (..)
+import Message
 import WebSocket
 
 -- Model
@@ -12,18 +14,37 @@ type alias Model = {
 	playerId : Int
 }
 
+init : (Model, Cmd Msg)
+init =
+	(Model "" -1, Cmd.none)
+
 type Msg =
-	OnMessage String
+	OnMessage String |
+	Join String
+
+-- Update
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg {address, playerId} =
 	case msg of
-		OnMessage str ->
-			(Model address )
+
+		Join handle ->
+
+		OnMessage json ->
+			case decodeString Message.message json of
+				Ok Message ->
+					-- TODO decompose join message
+				Err Reason ->
+					-- TODO show error message
+
+
+-- Subscriptions
 
 subscriptions : Model -> Sub Msg
 subscriptions {addres, _} =
 	WebSocket.listen address OnMessage
+
+-- Main
 
 main : Program (Maybe Model)
 main =
