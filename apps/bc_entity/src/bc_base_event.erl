d43db9323a7b76c0c26f11a28310809e3afdaeb1
jake <j.breindel2@outlook.com>
2016-07-26 19:24:26 -0400

    Added code to ensure that when a base is destroyed it contacts the fsm.

---
+++ apps/bc_entity/src/bc_base_event.erl
@@ -0,0 +1,30 @@
+
+-module(bc_base_entities).
+-behavior(gen_event).
+
+-export([init/1, handle_event/2]).
+
+%% internal state
+-record(state, {base_uuid, game, player}).
+
+%%====================================================================
+%% Public functions
+%%====================================================================
+
+init([BaseUuid, BcGame, BcPlayer]) ->
+	{ok, #state{base_uuid = BaseUuid, 
+				game = BcGame,
+				player = BcPlayer}}.
+
+handle_event({entitiy_died, BcEntity}, #state{base_uuid = BaseUuid, 
+											  game = BcGame, 
+											  player = BcPlayer} = State) ->
+	case BaseUuid =:= bc_entity:uuid(BcEntity) of
+		true ->
+			GameFsmPid = bc_game:fsm(BcGame),
+			PlayerId = bc_player:id(BcPlayer),
+			bc_game_fsm:player_out(GameFsmPid, PlayerId),
+			remove_handler;
+		false ->
+			{ok, State}
+	end.
--- apps/bc_entity/src/bc_entities.erl
+++ apps/bc_entity/src/bc_entities.erl
@@ -8,38 +8,63 @@
 							{heir, Heir, []}, 
 							{write_concurrency, true}]).
 
--export([init/1, insert_new/2, insert/2, query/2, delete/2]).
+-export([init/1, 
+		 table/1,
+		 event/1,
+		 insert_new/2, 
+		 insert/2, 
+		 query/2, 
+		 delete/2]).
+
+%%
+% @doc entities structure includes gen_event along with ets table.
+%%
+-type bc_entities() :: #{entities_event => pid(),
+						 entities_tab => ets:tid()}.
 
 %%====================================================================
 %% API functions
 %%====================================================================
 
--spec init(Heir :: pid()) -> ets:tab().
+-spec init(Heir :: pid()) -> bc_entities().
 init(Heir) ->
-	ets:new(entities, ?ETS_OPTIONS(Heir)).
+	#{entities_event => supervisor:start_child(Heir, #{
+		id => bc_entities_event,
+		start => {gen_event, start_link, []},
+		modules => [gen_event]
+	  }),
+	  entities_pid => ets:new(entities, ?ETS_OPTIONS(Heir))}.
 
--spec insert_new(Tab :: ets:tab(), 
+-spec table(BcEntities :: bc_entities()) -> ets:tid().
+table(#{entities_tab := Tab}) ->
+	Tab.
+
+-spec event(BcEntities :: bc_entities()) -> pid().
+event(#{entities_event := EntitiesEventPid}) ->
+	EntitiesEventPid.
+
+-spec insert_new(BcEntities :: bc_entities(), 
 				 BcEntity :: bc_entity:entity()) -> boolean().
-insert_new(Tab, BcEntity) ->
+insert_new(#{entities_tab := Tab}, BcEntity) ->
 	Row = bc_entity:to_tuple(BcEntity),
 	ets:insert_new(Tab, Row).
 
--spec insert(Tab :: ets:tab(), 
+-spec insert(BcEntities :: bc_entities(), 
 			 BcEntity :: bc_entity:entity()) -> true.
-insert(Tab, BcEntity) ->
+insert(#{entities_tab := Tab}, BcEntity) ->
 	Row = bc_entity:to_tuple(BcEntity),
 	ets:insert(Tab, Row).
 
--spec query(Tab :: ets:tab(), 
+-spec query(BcEntities :: bc_entities(), 
 			Uuid :: uuid:uuid()) -> [bc_entity:entity()].
-query(Tab, Uuid) when is_binary(Uuid) ->
-	query(Tab, [Uuid]);
-query(Tab, Uuids) when is_list(Uuids) ->
+query(BcEntities, Uuid) when is_binary(Uuid) ->
+	query(BcEntities, [Uuid]);
+query(#{entities_tab := Tab}, Uuids) when is_list(Uuids) ->
 	qlc:eval(qlc:q([bc_entity:from_tuple(BcEntityTuple) || 
 					  BcEntityTuple <- ets:table(Tab),
 					  lists:member(element(1, BcEntityTuple), Uuids)])).
 
--spec delete(Tab :: ets:tab(), 
+-spec delete(BcEntities :: bc_entities(), 
 			 Uuid :: uuid:uuid()) -> true.
-delete(Tab, Uuid) ->
+delete(#{entities_tab := Tab}, Uuid) ->
 	ets:delete(Tab, Uuid).
