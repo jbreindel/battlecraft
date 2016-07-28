060c291f06db918191f3f3c8ee6590cad68addfc
Jake Breindel <j.breindel2@outlook.com>
2016-07-28 15:16:13 +0000

    bc_vertex.erl created online with Bitbucket

---
+++ apps/bc_map/src/bc_vertex.erl
@@ -0,0 +1,24 @@
+
+-module(bc_vertex).
+
+-export([create/2, 
+		 row/1, 
+		 col/1]).
+		 
+%%
+% @doc vertex structure
+%%
+-type vertex() :: #{row => integer(), col => integer()}.
+
+-spec create(Row :: integer(), 
+			 Col :: integer()) -> vertex().
+create(Row, Col) ->
+	#{row => Row, col => Col}.
+	
+-spec row(BcVertex :: vertex()) -> integer().
+row(BcVertex) ->
+	maps:get(row, BcVertex).
+	
+-spec col(BcVertex :: vertex()) -> integer().
+col(BcVertex) ->
+	maps:get(col, BcVertex).
