7822f921233db0418bb6132947ce99274e495997
jake <j.breindel2@outlook.com>
2016-08-07 22:39:00 -0400

    Added more code for entity view.

--- apps/bc_web/priv/static/elm/Entity.elm
+++ apps/bc_web/priv/static/elm/Entity.elm
@@ -25,6 +25,7 @@ type EntityState = Standing | Moving | Attacking
 
 type alias Model = {
     entity : Entity,
+    vertexMatrix : Dict Int List Int
     orientation : Orientation,
     entityState : EntityState
 }
@@ -39,8 +40,58 @@ init entity =
         entityState = Standing
     }
 
--- view : Map.Model Model -> Collage.Form
--- view model TmxMap =
+-- View
+
+entityRowCount : Dict -> Int
+entityRowCount vertexMatrix =
+    let
+        rows = Dict.keys vertexMatrix
+    in
+        List.length rows
+
+entityHeight : TmxMap -> Dict -> Int
+entityHeight tmxMap vertexMatrix =
+    let
+        entityRows = entityRowCount vertexMatrix
+    in
+        entityRows * tmxMap.tileHeight
+
+entityColCount : Dict -> Int
+entityColCount vertexMatrix =
+    let
+        row = Dict.keys vertexMatrix
+                |> List.take 1
+
+        mbCols = Dict.get row vertexMatrix
+    in
+        case mbCols of
+            Just cols ->
+                List.length mbCols
+            Nothing ->
+                0
+
+entityWidth : TmxMap -> Dict -> Int
+entityWidth tmxMap vertexMatrix =
+    let
+        entityCol = entityColCount vertexMatrix
+    in
+        entityCol * tmxMap.tileWidth
+
+-- entityPosition : TmxMap -> Dict -> (Int, Int)
+-- entityPosition tmxMap vertexMatrix =
+--     let
+--         height = entityHeight tmxMap vertexMatrix
+--
+--         heightOffset = height / 2
+--
+--         width = entityWidth tmxMap vertexMatrix
+--
+--         widthOffset = entityWidth / 2
+--     in
+--
+
+-- view : Model -> TmxMap -> Collage.Form
+-- view model tmxMap =
 --     let
 --         entityType = model.entity.entityType
 --
@@ -48,7 +99,7 @@ init entity =
 --
 --         healthPct = model.entity.health / maxHealth
 --
---         vertices = model.entity.vertices
+--         entityWidth = entityWidth tmxMap.tileWidth model.vertexMatrix
 --     in
 --         case entityType of
 --
