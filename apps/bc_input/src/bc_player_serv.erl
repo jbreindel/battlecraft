3974aa0048de104ac3dc44597399dea74c380a7b
jake <j.breindel2@outlook.com>
2016-08-13 00:28:04 -0400

    Added vertex sort and more code to determine player number.

--- apps/bc_input/src/bc_player_serv.erl
+++ apps/bc_input/src/bc_player_serv.erl
@@ -12,6 +12,7 @@
 -record(state, {player_sup,
 				game,
 				player,
+				player_num,
 				gold,
 				map,
 				entities}).
@@ -39,3 +40,33 @@ init([BcPlayerSup, BcGame, BcPlayer, BcGoldFsm, BcMap, BcEntities]) ->
 				gold = BcGoldFsm,
 				map = BcMap,
 				entities = BcEntities}}.
+
+%%====================================================================
+%% Internal functions
+%%====================================================================
+
+vertex_sort(BcVertex1, BcVertex2) ->
+
+base_num(BaseBcVertices) ->
+	SortedBaseBcVertices = lists:sort(fun bc_vertex:sort/2, BaseBcVertices),
+	%% TODO determine base number
+	1.
+
+player_num(#state{player = BcPlayer,
+				  player_num = PlayerNum,
+				  map = BcMap, 
+				  entities = BcEntities} = State) ->
+	PlayerId = bc_player:id(BcPlayer),
+	BaseBcEntities = bc_entity:query_type(base, BcEntities),
+	case lists:filter(fun(BaseBcEntity) -> 
+						  PlayerId =:= bc_entity:player_id(BaseBcEntity) 
+					  end, BaseBcEntities) of
+		[] ->
+			undefined;
+		[PlayerBaseBcEntity] ->
+			Uuid = bc_entity:uuid(PlayerBaseBcEntity),
+			QueryResults = bc_map:query_collisions(Uuid, BcEntities),
+			BcVertices = lists:map(fun(#{vertex := BcVertex}) -> BcVertex end, QueryResults),
+			base_num(BcVertices)
+	end.
+
--- apps/bc_map/src/bc_vertex.erl
+++ apps/bc_map/src/bc_vertex.erl
@@ -4,7 +4,8 @@
 -export([init/2, 
 		 row/1, 
 		 col/1,
-		 to_tuple/1]).
+		 to_tuple/1,
+		 compare/2]).
 		 
 %%
 % @doc vertex structure
@@ -30,3 +31,31 @@ col(BcVertex) ->
 -spec to_tuple(BcVertex :: vertex()) -> tuple().
 to_tuple(#{row := Row, col := Col}) ->
 	{Row, Col}.
+
+compare_rows(Int1, Int2) when Int1 < Int2 ->
+	lt;
+compare_rows(Int1, Int2) when Int1 > Int2 ->
+	gt;
+compare(Int1, Int2) when Int1 == Int2 ->
+	equal.
+
+-spec compare(BcVertex1 :: vertex(), 
+			  BcVertex2 :: vertex()) -> boolean().
+compare(BcVertex1, BcVertex2) ->
+	Row1 = row(BcVertex1),
+	case row(BcVertex2) of
+		Row2 when Row1 < Row2 ->
+			true;
+		Row2 when Row1 > Row2 ->
+			false;
+		Row2 when Row1 == Row2 ->
+			Col1 = col(BcVertex1),
+			case col(BcVertex2) of
+				Col2 when Col1 < Col2 ->
+					true;
+				Col2 when Col1 > Col2 ->
+					false;
+				_ ->
+					true
+			end
+	end.
