c35e9a4be0aabd896b44316ea57f213e4a07704e
jake <j.breindel2@outlook.com>
2016-08-31 18:36:21 -0400

    Added utility module to help with spawning entities.

---
+++ apps/bc_entity/src/bc_entity_util.erl
@@ -0,0 +1,41 @@
+
+-module(bc_entity_util).
+
+-export([spawn_entity/7]).
+
+spawn_entity(BcCollision, BcPlayer, BcEntitySup, 
+			 BcEntityConfig, Orientation, BcMap, BcEntities) ->
+	case bc_map:insert_collision(BcMap, BcCollision) of
+		true ->
+			BcEntity = create_entity(BcCollision, BcPlayer, BcEntitySup, 
+									 BcEntityConfig, Orientation),
+			case bc_entities:insert_new(BcEntity, BcEntities) of
+				true ->
+					EntitiesEventPid = bc_entities:event(BcEntities),
+					gen_event:notify(EntitiesEventPid, {entity_spawned, BcEntity}),
+					{ok, BcEntity};
+				false ->
+					{error_entity, "Error inserting entity."}
+			end;
+		false ->
+			{error_collision, "Cannot insert collision."}
+	end.
+
+create_entity(BcCollision, BcPlayer, BcEntitySup, BcEntityConfig, Orientation) ->
+	Uuid = bc_collision:uuid(BcCollision),
+	BcVertices = bc_collision:vertices(BcCollision),
+	BaseUuidStr = uuid:uuid_to_string(Uuid),
+	PlayerId = bc_player:id(BcPlayer),
+	Team = bc_player:team(BcPlayer),
+	EntityType = bc_entity_config:entity_type(BcEntityConfig),
+	Health = bc_entity_config:health(BcEntityConfig),
+	BcEntity = bc_entity:init(BaseUuidStr, PlayerId, Team, EntityType, 
+				   			  Health, Health, Orientation, BcVertices).
+	{ok, BcAiFsm} = supervisor:start_child(BcEntitySup, #{
+		id => Uuid,
+		start => {bc_ai_fsm, start_link, [BcEntity, BcEntities, BcMap]},
+		restart => transient,
+		type => worker,
+		modules => [bc_ai_fsm]
+	}),
+	bc_entity:set_ai_fsm(BcAiFsm, BcEntity).
