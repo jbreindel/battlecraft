a2bbe7c5ea9be5e25cfd774af0aa88bc632134f0
jake <jake@diesel>
2016-09-13 20:56:09 -0400

    Added more callbacks for gen_events.

--- apps/bc_entity/src/bc_base_event.erl
+++ apps/bc_entity/src/bc_base_event.erl
@@ -3,7 +3,11 @@
 -behavior(gen_event).
 
 -export([init/1, 
-		 handle_event/2]).
+		 handle_event/2,
+		 handle_call/2, 
+		 handle_info/2, 
+		 terminate/2, 
+		 code_change/3]).
 
 %% internal state
 -record(state, {base_uuid, 
@@ -14,11 +18,26 @@
 %% Public functions
 %%====================================================================
 
+-spec init(InitArgs) -> Result when
+	InitArgs :: Args | {Args, Term :: term()},
+	Args :: term(),
+	Result :: {ok, State}
+			| {ok, State, hibernate}
+			| {error, Reason :: term()},
+	State :: term().
 init([BaseUuid, BcGame, BcPlayer]) ->
 	{ok, #state{base_uuid = BaseUuid, 
 				game = BcGame,
 				player = BcPlayer}}.
 
+-spec handle_event(Event :: term(), State :: term()) -> Result when
+	Result :: {ok, NewState}
+			| {ok, NewState, hibernate}
+			| {swap_handlers, Args1, NewState, Handler2, Args2}
+			| remove_handler,
+	NewState :: term(), Args1 :: term(), Args2 :: term(),
+	Handler2 :: Module2 | {Module2, Id :: term()},
+	Module2 :: atom().
 handle_event({entitiy_died, BcEntity}, #state{base_uuid = BaseUuid, 
 											  game = BcGame, 
 											  player = BcPlayer} = State) ->
@@ -33,3 +52,44 @@ handle_event({entitiy_died, BcEntity}, #state{base_uuid = BaseUuid,
 	end;
 handle_event(_, State) ->
 	{ok, State}.
+
+-spec handle_call(Request :: term(), State :: term()) -> Result when
+	Result :: {ok, Reply, NewState}
+			| {ok, Reply, NewState, hibernate}
+			| {swap_handler, Reply, Args1, NewState, Handler2, Args2}
+			| {remove_handler, Reply},
+	Reply :: term(),
+	NewState :: term(), Args1 :: term(), Args2 :: term(),
+	Handler2 :: Module2 | {Module2, Id :: term()},
+	Module2 :: atom().
+handle_call(Request, State) ->
+    Reply = ok,
+    {ok, Reply, State}.
+
+-spec handle_info(Info :: term(), State :: term()) -> Result when
+	Result :: {ok, NewState}
+			| {ok, NewState, hibernate}
+			| {swap_handler, Args1, NewState, Handler2, Args2}
+			| remove_handler,
+	NewState :: term(), Args1 :: term(), Args2 :: term(),
+	Handler2 :: Module2 | {Module2, Id :: term()},
+	Module2 :: atom().
+handle_info(Info, State) ->
+    {ok, State}.
+
+-spec terminate(Arg, State :: term()) -> term() when
+	Arg :: Args
+		| {stop, Reason}
+		| stop
+		| remove_handler
+		| {error, {'EXIT', Reason}}
+		| {error, Term :: term()},
+	Args :: term(), Reason :: term().
+terminate(Arg, State) ->
+    ok.
+
+-spec code_change(OldVsn, State :: term(), Extra :: term()) -> {ok, NewState :: term()} when
+	OldVsn :: Vsn | {down, Vsn},
+	Vsn :: term().
+code_change(OldVsn, State, Extra) ->
+    {ok, State}.
