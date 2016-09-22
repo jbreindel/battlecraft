7c3a4d527e658dc816c24c16f5291e0d83fa1c87
jake <jake@diesel>
2016-09-21 21:44:16 -0400

    Added more code for Spawn UI.

--- apps/bc_web/priv/static/elm/Spawn.elm
+++ apps/bc_web/priv/static/elm/Spawn.elm
@@ -6,7 +6,7 @@ module Spawn exposing (Effect(..),
                        update,
                        view)
 
-import Html exposing (Html, div, h5, p, img, text)
+import Html exposing (Html, div, h5, p, figure, a, img, text)
 import Html.Attributes exposing (class, src)
 import Effects exposing (Effects)
 import Keyboard.Extra as Keyboard
@@ -87,19 +87,66 @@ entityType keyboardModel =
     else
         Nothing
 
+-- View
+
 view : Model -> Html Msg
 view model =
     div [class "tile is-vertical is-2 box is-overlay spawn-content"] [
+
+        -- Heading
         div [class "columns"] [
-            div [class "column is-two-thirds is-flex spawn-header"] [
+            div [class "column is-two-thirds is-flex is-vcentered"] [
                 h5 [class "title is-5"] [
                     text "Spawn"
                 ]
             ],
-            div [class "column is-flex spawn-gold"] [
+            div [class "column is-flex is-vcentered"] [
                 img [src "/static/assets/interface/upgrades/building.PNG"] [],
                 p [class "gold-amount"] [
                     text (toString model.gold)
+                ]
+            ]
+        ],
+
+        -- Entities
+        div [class "columns"] [
+            div [class "column is-half is-flex is-vcentered"] [
+                div [class "columns"] [
+                    div [class "column is-flex is-vcentered"] [
+                        a [class "button is-medium"] [
+                            h5 [class "title is-5 entity-key"] [
+                                text "A"
+                            ]
+                        ]
+                    ],
+                    div [class "column is-flex is-vcentered"] [
+                        figure [class "image is-48x48"] [
+                            img [class "entity", src "/static/assets/units/b_champion/champion_stand_1.PNG"] []
+                        ],
+                        p [] [
+                            text "100"
+                        ]
+                    ]
+                ]
+            ],
+            div [class "column is-half is-flex is-vcentered"] [
+                div [class "columns"] [
+                    div [class "column is-flex is-vcentered"] [
+                        a [class "button is-medium"] [
+                            h5 [class "title is-5 entity-key"] [
+                                text "S"
+                            ]
+                        ]
+                    ],
+                    div [class "column is-flex is-vcentered"] [
+                        figure [class "image is-48x48"] [
+                            img [class "entity", src "/static/assets/units/b_demon/demon_stand_1.PNG"] []
+                        ],
+                        div [class "is-clearfix"] [],
+                        p [] [
+                            text "300"
+                        ]
+                    ]
                 ]
             ]
         ]
