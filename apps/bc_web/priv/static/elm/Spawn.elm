2399a00f284842cfc1524a2cfc2c3c7e6599f465
jake <j.breindel2@outlook.com>
2016-09-22 21:38:54 -0400

    Added code to better display spawn UI.

--- apps/bc_web/priv/static/elm/Spawn.elm
+++ apps/bc_web/priv/static/elm/Spawn.elm
@@ -8,6 +8,7 @@ module Spawn exposing (Effect(..),
 
 import Html exposing (Html, div, h5, p, figure, a, img, text)
 import Html.Attributes exposing (class, src)
+import Html.Lazy exposing (lazy)
 import Effects exposing (Effects)
 import Keyboard.Extra as Keyboard
 import Json.Encode exposing (encode)
@@ -91,54 +92,146 @@ entityType keyboardModel =
 
 view : Model -> Html Msg
 view model =
+    lazy spawnTile model
+
+spawnTile : Model -> Html Msg
+spawnTile model =
     div [class "tile is-vertical is-2 box is-overlay spawn-content"] [
 
         -- Heading
         div [class "columns"] [
-            div [class "column is-two-thirds is-flex is-vcentered"] [
-                h5 [class "title is-5"] [
-                    text "Spawn"
-                ]
-            ],
-            div [class "column is-flex is-vcentered"] [
-                img [src "/static/assets/interface/upgrades/building.PNG"] [],
-                p [class "gold-amount"] [
-                    text (toString model.gold)
-                ]
-            ]
+            spawnTitle,
+            goldColumn model
         ],
 
         -- Entities
         div [class "columns"] [
-            div [class "column is-half is-flex is-vcentered"] [
-                div [class "columns is-gapless"] [
-                    div [class "column is-flex is-vcentered"] [
-                        a [class "button is-medium"] [
-                            h5 [class "title is-5 entity-key"] [
-                                text "A"
-                            ]
-                        ]
-                    ],
-                    div [class "column is-flex is-vcentered"] [
-                        figure [class "image is-48x48 entity-image"] [
-                            img [class "entity", src "/static/assets/units/b_champion/champion_stand_1.PNG"] []
-                        ]
+            championColumn,
+            demonColumn
+        ],
+
+        div [class "columns"] [
+            romanGuardColumn,
+            chaosRiderColumn
+        ]
+    ]
+
+spawnTitle : Html Msg
+spawnTitle =
+    div [class "column is-two-thirds is-flex is-vcentered"] [
+        h5 [class "title is-5"] [
+            text "Spawn"
+        ]
+    ]
+
+goldColumn : Model -> Html Msg
+goldColumn model =
+    div [class "column is-flex is-vcentered"] [
+        img [src "/static/assets/interface/upgrades/building.PNG"] [],
+        p [class "gold-amount"] [
+            text (toString model.gold)
+        ]
+    ]
+
+
+championColumn : Html Msg
+championColumn =
+    div [class "column is-half is-flex is-vcentered"] [
+        div [class "columns is-gapless"] [
+            div [class "column is-flex is-vcentered"] [
+                a [class "button is-medium"] [
+                    h5 [class "title is-5 entity-key"] [
+                        text "A"
                     ]
                 ]
             ],
-            div [class "column is-half is-flex is-vcentered"] [
-                div [class "columns is-gapless"] [
-                    div [class "column is-flex is-vcentered"] [
-                        a [class "button is-medium"] [
-                            h5 [class "title is-5 entity-key"] [
-                                text "S"
-                            ]
+            div [class "column is-flex is-vcentered"] [
+                div [class "entity-info"] [
+                    div [class "columns is-gapless is-marginless"] [
+                        figure [class "image is-48x48 entity-image"] [
+                            img [class "entity", src "/static/assets/units/b_champion/champion_stand_1.PNG"] []
                         ]
                     ],
-                    div [class "column is-flex is-vcentered"] [
+                    div [class "columns is-gapless is-marginless"] [
+                        p [class "is-margin-auto"] [text "100"]
+                    ]
+                ]
+            ]
+        ]
+    ]
+
+demonColumn : Html Msg
+demonColumn =
+    div [class "column is-half is-flex is-vcentered"] [
+        div [class "columns is-gapless"] [
+            div [class "column is-flex is-vcentered"] [
+                a [class "button is-medium"] [
+                    h5 [class "title is-5 entity-key"] [
+                        text "S"
+                    ]
+                ]
+            ],
+            div [class "column is-flex is-vcentered"] [
+                div [class "entity-info"] [
+                    div [class "columns is-gapless is-marginless"] [
                         figure [class "image is-48x48 entity-image"] [
                             img [class "entity", src "/static/assets/units/b_demon/demon_stand_1.PNG"] []
                         ]
+                    ],
+                    div [class "columns is-gapless is-marginless"] [
+                        p [class "is-margin-auto"] [text "300"]
+                    ]
+                ]
+            ]
+        ]
+    ]
+
+romanGuardColumn : Html Msg
+romanGuardColumn =
+    div [class "column is-half is-flex is-vcentered"] [
+        div [class "columns is-gapless"] [
+            div [class "column is-flex is-vcentered"] [
+                a [class "button is-medium"] [
+                    h5 [class "title is-5 entity-key"] [
+                        text "D"
+                    ]
+                ]
+            ],
+            div [class "column is-flex is-vcentered"] [
+                div [class "entity-info"] [
+                    div [class "columns is-gapless is-marginless"] [
+                        figure [class "image is-48x48 entity-image"] [
+                            img [class "entity", src "/static/assets/units/r_roman_guard/stand.PNG"] []
+                        ]
+                    ],
+                    div [class "columns is-gapless is-marginless"] [
+                        p [class "is-margin-auto"] [text "500"]
+                    ]
+                ]
+            ]
+        ]
+    ]
+
+chaosRiderColumn : Html Msg
+chaosRiderColumn =
+    div [class "column is-half is-flex is-vcentered"] [
+        div [class "columns is-gapless"] [
+            div [class "column is-flex is-vcentered"] [
+                a [class "button is-medium"] [
+                    h5 [class "title is-5 entity-key"] [
+                        text "W"
+                    ]
+                ]
+            ],
+            div [class "column is-flex is-vcentered"] [
+                div [class "entity-info"] [
+                    div [class "columns is-gapless is-marginless"] [
+                        figure [class "image is-48x48 entity-image"] [
+                            img [class "entity", src "/static/assets/units/b_chaos_rider/chaosreiter_walk_2.gif"] []
+                        ]
+                    ],
+                    div [class "columns is-gapless is-marginless"] [
+                        p [class "is-margin-auto"] [text "1000"]
                     ]
                 ]
             ]
