dacf3472858d1f7314381d7d717e5d50fcf6dda9
Jake Breindel <j.breindel2@outlook.com>
2016-10-07 17:35:32 +0000

    Added more code to add time gamed events.

--- apps/bc_web/priv/static/elm/GameCenter.elm
+++ apps/bc_web/priv/static/elm/GameCenter.elm
@@ -2,8 +2,11 @@
 module GameCenter exposing (Effect(..),
                             Msg(..),
                             Model,
-                            init)
+                            init,
+                            update)
 
+import Dict exposing (Dict)
+import Time exposing (Time)
 import Effects exposing (Effects)
 
 -- Local imports
@@ -13,21 +16,78 @@ import GameEvent exposing (GameEvent)
 -- Actions
 
 type Effect =
-    UpdateGameState GameState
+    UpdateGameState GameState |
+    PerformCmd (Cmd Msg)
 
 type Msg = 
-    GameEv GameEvent
+    ReceiveGameEv GameEvent |
+    AddTimedGameEv (Time, GameEvent) |
+    NoOp
 
 -- Model
     
 type alias Model = {
-    players : List Players,
-    messages : List GameEvent
+    players : Dict Int Player,
+    timedGameEvents : List (Time, GameEvent)
 }
 
 init : Effects Model Effect
 init =
     Effects.return {
-        players = [],
-        messages = []
+        players = Dict.empty,
+        timedGameEvents = []
     }
+
+-- Update
+
+update : Msg -> Model -> Effects Model Effect
+update msg model =
+    case msg of
+    
+        ReceiveGameEv gameEvent ->
+            onReceiveGameEvent gameEvent model
+            
+        AddTimedGameEv timedGameEvent ->
+            onAddTimedGameEvent timedGameEvent model
+            
+onReceiveGameEvent : GameEvent -> Model -> Effects Model Effect
+onReceiveGameEvent gameEvent model =
+    let
+       cmd =  gameEventCmd gameEvent
+    in
+        case gameEvent of
+    
+            PlayerEv playerEvent ->
+                let
+                    id = updatedPlayer.id
+                
+                    updatedPlayer = playerEvent.player
+                
+                    updatedPlayers = 
+                        Dict.insert id updatedPlayer model.players
+                in
+                    Effects.init {model |
+                        players = updatedPlayers
+                    } [PerformCmd cmd]
+                
+            _ ->
+                Effects.init model [PerformCmd cmd]
+
+onAddTimedGameEvent : (Time, GameEvent) -> Model -> Effects Model Effect
+onAddTimedGameEvent timeGameEvent model =
+    let
+        updatedTimedGameEvents = 
+            model.timedGameEvents ++ [timedGameEvent]
+    in
+        Effects.return {model |
+            timedGameEvents = updatedTimedGameEvents
+        }
+
+gameEventCmd : GameEvent -> Cmd Msg
+gameEventCmd gameEvent =
+    Time.now `Task.andThen` (
+        \time -> 
+            Task.succeed (time, playerEvent)
+    )
+    |> Task.perform NoOp AddGameEv
+    
